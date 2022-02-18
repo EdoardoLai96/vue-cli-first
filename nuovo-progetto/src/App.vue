9a1ad35c59e073f22c88d579552c3baf46daa29d
Edoardo Lai <edoardolai96@gmail.com>
2022-02-18 15:42:12 +0100

    Progetto terminato

--- nuovo-progetto/src/App.vue
+++ nuovo-progetto/src/App.vue
@@ -1,17 +1,22 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Welcome to Your Vue.js App"/>,
+    <NuovoTitolo>
+
+    </NuovoTitolo>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import NuovoTitolo from './components/NuovoTitolo.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    NuovoTitolo
   }
 }
 </script>
